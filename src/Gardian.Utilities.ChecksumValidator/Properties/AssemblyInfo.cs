83cf822ccf3f4a051bbd0750f88ecc6250cb3369
Milan Gardian <mgardian@gmail.com>
2009-04-16 20:57:32 -0600

    Update program information (exe properties) - version, title, etc.

--- src/Gardian.Utilities.ChecksumValidator/Properties/AssemblyInfo.cs
+++ src/Gardian.Utilities.ChecksumValidator/Properties/AssemblyInfo.cs
@@ -1,36 +1,28 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Sha1Check")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Sha1Check")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("ChecksumValidator")]
+[assembly: AssemblyDescription("ChecksumValidator: GUI application to compute checksum of a specified file and validate it against expected checksum")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCopyright("Copyright (C) 2009 Milan Gardian. All rights reserved.")]
 [assembly: ComVisible(false)]
+[assembly: Guid("10ABE120-00E5-43FB-8101-16ACA2A31A04")]
+[assembly: CLSCompliant(true)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("10abe120-00e5-43fb-8101-16aca2a31a04")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("ChecksumValidator v1.0.0 "
+#if DEBUG
+	+ "(debug)"
+#else
+	+ "(release)"
+#endif
+)]
+[assembly: AssemblyConfiguration(
+#if DEBUG
+	"DEBUG"
+#else
+	"RELEASE"
+#endif
+)]
